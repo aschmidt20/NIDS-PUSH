ce1b04d7ea56f798a554b9241a63a2f89aea4d9e
Tyler Will <twill4398@gmail.com>
2017-11-15 21:26:04 -0500

    Objective functions and mixing matrices
    Translated the matlab code for creating objective functions and defining the mixing matrices.  
    Some odd quirks with indexing/ranges in Matlab vs Python make for bad practice- should probably clean those up at some point.

--- extrapush-subgradpush-LS-reg.py
+++ extrapush-subgradpush-LS-reg.py
@@ -1,5 +1,7 @@
-import numpy as np
+from numpy import *
+from math import sqrt
 # -*- coding: utf-8 -*-
+
 """
 Python translation from Matlab for extrapush versus Subgradient push for least squares regression
 - minimize F(x) = f1(x)+f2(x)+f3(x)+f4(x)+f5(x)
@@ -7,11 +9,71 @@ Python translation from Matlab for extrapush versus Subgradient push for least s
 - Network: 5 nodes, strongly connected
 """
 
-# Gradient for Least Squares
-# f_i(x) = 1/2*norm(Ax-b)^2
+"""
+Authors: Tyler Will, Andrew Schmidt
+"""
+
+
 def LS_grad(x,A,b):
- return np.transpose(A).matmul(A.matmul(x)-b)
+    """
+    Gradient for least squares
+    Calculates f_i(x) = (1/2) * norm(Ax-b)^2
+    TODO: If we make the assumption that A is a matrix, we can clean
+    up the expression below 
+    """
+    
+    return transpose(A)*(A*x-b)
 
+"""
+Defining objective functions
+"""
 
+m = 100
+p = 256
+k = 40
 
+# Matlab uses randperm inclusive; range is half open
+d = random.permutation(range(1, p + 1))
+
+# True signal
+xs = np.zeros((p, 1))
+
+# We have to subtract one from the left hand side because indexing starts at 0
+# Otherwise we get an error because 256 is selected (at times)
+xs[d[0:k] - 1] = 2 * random.randn(k, 1)
+
+sigma = 0.01
+
+B1 = matrix(0.1 * random.randn(m, p))
+B2 = matrix(0.1 * random.randn(m, p))
+B3 = matrix(0.1 * random.randn(m, p))
+B4 = matrix(0.1 * random.randn(m, p))
+B5 = matrix(0.1 * random.randn(m, p))
+
+b1 = B1 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
+b2 = B2 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
+b3 = B3 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
+b4 = B4 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
+b5 = B5 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
+
+# At this point in the script, b1-b5 look similar to how they do in the matlab 
+# script 
+
+"""
+Mixing matrices of the connected network 
+"""
+
+A = matrix([[1/4, 1/4, 0, 1/2, 0], [1/4, 1/4, 0, 0, 1/3], [1/4, 0, 1/2, 0, 1/3], 
+            [0, 1/4, 0, 1/2, 0], [1/4, 1/4, 1/2, 0, 1/3]])
+n = shape(A)[0]
+A1 = (A + identity(n))/2
+
+##############################################################################
+B = (transpose(B1) * B1 + transpose(B2) * B2 + transpose(B3) * B3 + 
+     transpose(B4) * B4 + transpose(B5) * B5)
+b = (transpose(B1) * b1 + transpose(B2) * b2 + transpose(B3) * b3 + 
+     transpose(B4) * b4 + transpose(B5) * b5)
+
+# Need to verify that this is the proper division - I have a feeling it isn't.  
+Opt_x = B/b 
 
