8f356c518efefbe345fde72c1e3259905dacb712
Tyler Will <twill4398@gmail.com>
2017-11-15 22:27:15 -0500

    More initialization, fixed grad func
    Fixed the top gradient function once it was apparent that it was not functional, continued adding parameters for initialization of Extra-Push

--- extrapush-subgradpush-LS-reg.py
+++ extrapush-subgradpush-LS-reg.py
@@ -18,11 +18,10 @@ def LS_grad(x,A,b):
     """
     Gradient for least squares
     Calculates f_i(x) = (1/2) * norm(Ax-b)^2
-    TODO: If we make the assumption that A is a matrix, we can clean
-    up the expression below 
+    Nasty syntax, but pure * notation was not working
     """
     
-    return transpose(A)*(A*x-b)
+    return matmul(transpose(A), matmul(A, x)-b)
 
 """
 Defining objective functions
@@ -59,6 +58,9 @@ b5 = B5 * xs + ((sigma/sqrt(m)) * random.randn(m,1))
 # At this point in the script, b1-b5 look similar to how they do in the matlab 
 # script 
 
+
+
+
 """
 Mixing matrices of the connected network 
 """
@@ -74,6 +76,40 @@ B = (transpose(B1) * B1 + transpose(B2) * B2 + transpose(B3) * B3 +
 b = (transpose(B1) * b1 + transpose(B2) * b2 + transpose(B3) * b3 + 
      transpose(B4) * b4 + transpose(B5) * b5)
 
-# Need to verify that this is the proper division - I have a feeling it isn't.  
-Opt_x = B/b 
+# The B\b syntax used in matlab does not exist  
+Opt_x = linalg.solve(B, b)
+
+MaxIter = 10000
+
+
+
+
+"""
+Initialization of EXTRA-Push
+"""
+# Step size parameter for EXTRA-Push
+alpha0 = 0.1
+alpha1 = 0.02
+
+# Initialization of w-sequence 
+w0 = ones((n,p))
+
+# Random initialization of sequence z 
+z0 = random.randn(n, p);
+x0 = z0
+w1 = matmul(A, w0)
+
+grad01 = LS_grad(transpose(x0[0]), B1, b1)
+grad02 = LS_grad(transpose(x0[1]), B2, b2)
+grad03 = LS_grad(transpose(x0[2]), B3, b3)
+grad04 = LS_grad(transpose(x0[3]), B4, b4)
+grad05 = LS_grad(transpose(x0[4]), B5, b5)
+
+myfunMD_grad0 = [[transpose(grad01)], [transpose(grad02)], [transpose(grad03)], 
+                  [transpose(grad04)], [transpose(grad05)]]
+
+z00 = z0
+z01 = z0
+
+
 
